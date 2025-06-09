9568386102dd2c395a0695c6a971ad18598a30b5
jGomes <jrgomes1911@hotmail.com>
2025-06-09 17:39:59 +0100

    change viginere encode to use full table range. Improve short keyStream behavior.

--- substitution/monomic_polyalpha/vigenere.py
+++ substitution/monomic_polyalpha/vigenere.py
@@ -6,16 +6,31 @@ vigenere_table = [
     for i in range(26)
 ]
 
-def encode(text: str = "") -> str:
+def encode(text: str = "", keyStream: str = "") -> str:
     result = []
+    filtered_key = [k for k in keyStream if k.isalpha()]
+    key_len = len(filtered_key)
+    key_index = 0
 
     for c in text:
-        result.append(_search_viginere_table(c))
+        if c.isalpha():
+            key = filtered_key[key_index % key_len]
+            result.append(_search_viginere_table(c, key))
+            key_index += 1
+        else:
+            result.append(c)
 
     return "".join(result)
 
-def _search_viginere_table(c):
-    pos = ord(c.upper()) - ord('A') 
-    if not (0 <= pos < 26) or not c.isalpha():
+def _search_viginere_table(c, key):
+    # Compute char and key positions
+    posChar = ord(c.upper()) - ord('A')
+    posKey = ord(key.upper()) - ord('A')
+    
+    # If char is not a letter or in range return char itself
+    if not (0 <= posChar < 26) or not c.isalpha():
         return c
-    return vigenere_table[pos][pos]
+    if not (0 <= posKey < 26) or not key.isalpha():
+        return c
+    # return table position
+    return vigenere_table[posChar][posKey]
