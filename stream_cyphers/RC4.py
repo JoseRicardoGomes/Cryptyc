5a17e9e69615848ca506cb133ef1cfc817dab5a9
jGomes <jrgomes1911@hotmail.com>
2025-06-12 23:24:15 +0100

    Document RC4.py. Add Missing file extensions.

--- stream_cyphers/RC4.py
+++ stream_cyphers/RC4.py
@@ -1,5 +1,8 @@
 # stream_cyphers.RC4
 
+'''
+Generate classic RC4 256bit state array.
+'''
 def init_state_array(key: str = "") -> list:
     key_bytes = [ord(char) for char in key]
     key_len = len(key_bytes)
@@ -14,6 +17,9 @@ def init_state_array(key: str = "") -> list:
 
     return state
 
+'''
+Generate keystream.
+'''
 def generate_keystream(state: list, len: int) -> list:
     i = j = 0
     keystream = []
@@ -28,12 +34,18 @@ def generate_keystream(state: list, len: int) -> list:
 
     return keystream 
 
+'''
+RC4 encript message using key.
+'''
 def encrypt(msg: str = "", key: str = "") -> bytes:
     sarray = init_state_array(key)
     ks = generate_keystream(sarray, len(msg))
     encrypted = bytes([ord(char) ^ ks_byte for char, ks_byte in zip(msg, ks)])
     return encrypted
 
+'''
+Decrypt bytes msg using key. Parameter msg should be passed as bytes.fromhex(str) or directly as byte stream.
+'''
 def decrypt(msg: str = "", key: str = "") -> str:
     sarray = init_state_array(key)
     ks = generate_keystream(sarray, len(msg))
