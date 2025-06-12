e59441d836c7bb0e1024462f49042034be9e3144
jGomes <jrgomes1911@hotmail.com>
2025-06-12 23:06:23 +0100

    Add folder for stream cyphers. Add files for RC4, PRGA and KSA. Implement RC4 encrypt/decrypt.

---
+++ stream_cyphers/RC4.py
@@ -0,0 +1,41 @@
+# stream_cyphers.RC4
+
+def init_state_array(key: str = "") -> list:
+    key_bytes = [ord(char) for char in key]
+    key_len = len(key_bytes)
+
+    state = list(range(256))
+
+    j = 0
+
+    for i in range(256):
+        j = (j + state[i] + key_bytes[i % key_len]) % 256
+        state[i], state[j] = state[j], state[i]
+
+    return state
+
+def generate_keystream(state: list, len: int) -> list:
+    i = j = 0
+    keystream = []
+
+    for _ in range(len):
+        i = (i + 1) % 256
+        j = (j + state[i]) % 256
+        state[i], state[j] = state[j], state[i]
+
+        t = (state[i] + state[j]) % 256
+        keystream.append(state[t])
+
+    return keystream 
+
+def encrypt(msg: str = "", key: str = "") -> bytes:
+    sarray = init_state_array(key)
+    ks = generate_keystream(sarray, len(msg))
+    encrypted = bytes([ord(char) ^ ks_byte for char, ks_byte in zip(msg, ks)])
+    return encrypted
+
+def decrypt(msg: str = "", key: str = "") -> str:
+    sarray = init_state_array(key)
+    ks = generate_keystream(sarray, len(msg))
+    decrypted = bytes([byte ^ ks_byte for byte, ks_byte in zip(msg, ks)])
+    return decrypted.decode('latin1')
